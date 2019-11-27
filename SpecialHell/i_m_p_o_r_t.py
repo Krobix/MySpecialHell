83e7020adbd50bec436125c2868760617e1c40fe
Krobix <45689440+Krobix@users.noreply.github.com>
2019-11-27 11:04:14 -0500

    Add files via upload

---
+++ SpecialHell/i_m_p_o_r_t.py
@@ -0,0 +1,20 @@
+import sys as _420_69
+import types as mmm_some_nice_booty
+
+yeet = mmm_some_nice_booty.ModuleType("yeet")
+_420_69.modules["yeet"] = yeet
+
+x = 0
+
+while x < 50:
+	yaYA = mmm_some_nice_booty.ModuleType(f"yeet{x}")
+	_420_69.modules[f"yeet.yeet{x}"] = yaYA
+	x += 1
+
+from yeet import (yeet0, yeet1, yeet2, yeet3, yeet4, yeet5, yeet6, yeet7, yeet8, yeet9, yeet10, yeet11, 
+yeet12, yeet13, yeet14, yeet15, yeet16, yeet17, yeet18, yeet19, yeet20, yeet21, yeet22, yeet23, yeet24, 
+yeet25, yeet26, yeet27, yeet28, yeet29, yeet30, yeet31, yeet32, yeet33, yeet34, yeet35, yeet36, yeet37, 
+yeet38, yeet39, yeet40, yeet41, yeet42, yeet43, yeet44, yeet45, yeet46, yeet47, yeet48, yeet49)
+
+yeet0.f_a_r_t_s = 1000000
+yeet5.aaaaa = yeet
---
+++ SpecialHell/we_dont_need_builtins.py
@@ -0,0 +1,15 @@
+import sys as fart, types as fart2
+
+def new_import_LOL(name):
+	mod = fart2.ModuleType(name)
+	fart.modules[name] = mod
+	while x < 50:
+		mod2 = fart2.ModuleType(f"yeet{x}")
+		fart.modules[f"{name}.yeet{x}"] = yaYA
+		x += 1
+
+__import__ = new_import_LOL
+
+import i_m_p_o_r_t
+
+print(i_m_p_o_r_t.__dict__)
