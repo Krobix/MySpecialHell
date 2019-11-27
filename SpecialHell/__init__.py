42787a5f3571d575266dbd950ad7910d41759c3c
Krobix <45689440+Krobix@users.noreply.github.com>
2019-11-27 10:04:22 -0500

    Add files via upload

---
+++ SpecialHell/__init__.py
@@ -0,0 +1,19 @@
+import classes as xXNoobSlayerXx
+MY_C0N2T4NT = "f to pay respects"
+
+def BADLYNAMEDFUNCTION(x):
+	print(x)
+	return BADLYNAMEDFUNCTION
+
+def DO_s0m3tHiNG():
+	return None
+	
+TOTALLY_VERY_VERY_REAL_FUNCTION = xXNoobSlayerXx.FUNCTION_SIMULATOR_2019("""
+import random, sys, ctypes, types, urllib3
+print('hallo')
+""")
+	
+if __name__ != "farts LOLLL" and __name__ == "__main__":	
+	BADLYNAMEDFUNCTION(xXNoobSlayerXx.abcdefghijklmnopqrstuvwxyz())(BADLYNAMEDFUNCTION(MY_C0N2T4NT))
+	IWontRememberThisNameAtAllLmaoXd = TOTALLY_VERY_VERY_REAL_FUNCTION()
+	
---
+++ SpecialHell/classes.py
@@ -0,0 +1,35 @@
+import types as MY_BIG_BOOTY
+
+class AaAaAaAaAAaAAaAaAa:
+	def __init__(self):
+		self.this = None
+		self.self = None
+
+class abcdefghijklmnopqrstuvwxyz:
+	def __init__(self):
+		this = AaAaAaAaAAaAAaAaAa()
+		self.self = this
+		this.this = self
+		self.this = this
+		this.self = self.self
+		this.self.self.this = self
+		self.this.this.self.this.self.this = self.__class__
+		
+	def no_u(self):
+		return self.this.self.this.NoU()
+		
+	def NoU(self):
+		return self.self.this.this.no_u()
+		
+	def __str__(self):
+		return str(self.__dict__)
+		
+class FUNCTION_SIMULATOR_2019:
+	def __init__(self, code):
+		self.code = code
+		self.VERY_SECRET_ATTRIBUTE_THAT_YOU_WILL_NEVER_FIND_AS_IT_IS_A_SECRET = MY_BIG_BOOTY.FunctionType(compile(code, 
+		"<FuncSim2019>", "exec"), globals())
+		
+	def __call__(self, *args):
+		self.VERY_SECRET_ATTRIBUTE_THAT_YOU_WILL_NEVER_FIND_AS_IT_IS_A_SECRET(*args)
+		
