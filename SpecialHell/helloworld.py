747cb1aba94e3c9266fa2fa1db44806ebd947eb8
Krobix <45689440+Krobix@users.noreply.github.com>
2019-12-17 11:16:50 -0500

    Add files via upload

---
+++ SpecialHell/helloworld.py
@@ -0,0 +1,14 @@
+#my first code lol xd hahaha its good right? jk i know it is cause im always right
+import copy as EVERYTIME_WE_TOUCH_I_GET_THIS_FEELING_AND_EVERYTIME_WE_KISS_I_SWEAR_I_CAN_FLY
+PRINT_2_ELECTRIC_BOOGALOO = EVERYTIME_WE_TOUCH_I_GET_THIS_FEELING_AND_EVERYTIME_WE_KISS_I_SWEAR_I_CAN_FLY.copy(print)
+
+class SAY_SOMETHING_OBJECT:
+	def __init__(self):
+		self.WORDS = "something"
+		
+	def __call__(self, word):
+		PRINT_2_ELECTRIC_BOOGALOO(word)
+		
+class SAY_HELLO_OBJECT(SAY_SOMETHING_OBJECT):
+	def __init__(self):
+		self.WORDS = "Hello, World!"
